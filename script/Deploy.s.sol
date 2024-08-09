a5cd2c56e3f6f9bfff78b9597a29522576156887
James Osunkiyesi <jaydhales@gmail.com>
2024-08-09 12:23:24 +0100

    setup

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,14 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "forge-ctf/CTFDeployment.sol";
+import "forge-std/Script.sol";
 
 import "src/Challenge.sol";
 import "src/SpaceBank.sol";
 
-contract Deploy is CTFDeployment {
-    function deploy(address system, address) internal override returns (address challenge) {
-        vm.startBroadcast(system);
+contract Deploy is Script {
+    function run() external returns (address challenge) {
+        vm.startBroadcast();
 
         SpaceToken token = new SpaceToken();
 
